13effab5b817b6031a85970c046a6459ac8b0024
Vladimir Kamuz <v.kamuz@gmail.com>
2022-09-20 22:47:49 +0300

    Reformat code

--- index.php
+++ index.php
@@ -4,8 +4,8 @@ require_once 'config/init.php';
 
 $job = new Job;
 $template = new Template('templates/frontpage.php');
-
 $category = isset($_GET['category']) ? $_GET['category'] : null;
+$template->categories = $job->getCategories();
 
 if ( $category ) {
 	$template->title = $job->getCategoryById($category)->name;
@@ -14,7 +14,5 @@ if ( $category ) {
 	$template->title = 'Latest Jobs';
 	$template->jobs = $job->getAllJobs();
 }
-
-$template->categories = $job->getCategories();
 
 echo $template;
